85d39cf726954667eb94fe64a373add1a15e9244
JamesStelle <3445490241@qq.com>
2026-01-02 05:02:09 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var roleHarvester = require('role.harvester');
+
+module.exports.loop = function () {
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        roleHarvester.run(creep);
+    }
+}
+
+//Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Harvester1' );
+//Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Harvester2' );
