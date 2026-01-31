0607657ef82fe889649e88c2c5a6b29d41f867f0
StarJim <3445490241@qq.com>
2026-01-31 14:30:15 +0800

    一级房

--- JamesStelle-V1/Tower.js
+++ JamesStelle-V1/Tower.js
@@ -1,18 +1,23 @@
 module.exports = {
-    run:function(){
-    // 控制防御塔
-        var tower = Game.getObjectById('68ea8e587e3d3a020818c7f1');
-        if(tower) {
+    run: function() {
+        // 获取房间内所有防御塔
+        var towers = _.filter(Game.structures, s => s.structureType === STRUCTURE_TOWER);
+        
+        // 遍历每个防御塔
+        for (var i = 0; i < towers.length; i++) {
+            var tower = towers[i];
+            
+            // 修复受损结构
             var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
                 filter: (structure) => structure.hits < structure.hitsMax
             });
-            // 修复受损结构
-            if(closestDamagedStructure) {
+            if (closestDamagedStructure) {
                 tower.repair(closestDamagedStructure);
             }
+            
             // 攻击最近的敌对爬虫
             var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-            if(closestHostile) {
+            if (closestHostile) {
                 tower.attack(closestHostile);
             }
         }
